7bb0c5655e480673a6671b704046ca78a504d79a
Deanna <bridge.dea@gmail.com>
2019-05-13 04:55:26 +0000

    Fixed: Branding link to correctly divert logged in user to ther homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'MasterMind') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,28 +21,20 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-<div id="laravel">
+<div id="app">
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
         <div class="container">
 
 
             @guest
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'MasterMind') }}
+                    {{ config('app.name', 'Laravel') }}
                 </a>
             @else
                 <a class="navbar-brand" href="{{ route('home') }}">
                     Home
                 </a>
-                {{--<a>--}}
-                {{--@if (Auth::user()->VIP === '0')--}}
-                    {{--<h3 style="color:#38c172">Not VIP </h3>--}}
-                {{--@else--}}
-                    {{--<h3 style="color:#38c172">VIP</h3>--}}
-                {{--@endif--}}
-                {{--</a>--}}
             @endguest
-
 
 
 
@@ -58,49 +50,15 @@
                 </ul>
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                My Account <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                @if (Auth::user()->profile)
-                                    <a class="dropdown-item" href="{{ route('profile.show', ['user_id' => Auth::user()->id,'profile_id' => Auth::user()->profile->id]) }}">My Profile</a>
-
-                                @else
-                                    <a class="dropdown-item" href="{{ route('profile.create', ['user_id' => Auth::user()->id]) }}">Create Profile</a>
-                                @endif
-
-
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
             </div>
         </div>
     </nav>
 
     <main class="py-4">
         <div class="col-12">
-            @include('flash.error')
-            @include('flash.messages')
-            @include('flash.status')
+        @include('flash.error')
+        @include('flash.messages')
+        @include('flash.status')
         </div>
         @yield('content')
     </main>
