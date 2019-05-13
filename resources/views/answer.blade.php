5f89fa98581ac8d17d3e8134205d0364418a2b11
Deanna <bridge.dea@gmail.com>
2019-05-13 05:52:20 +0000

    Updated answer.blade file and other views

--- resources/views/answer.blade.php
+++ resources/views/answer.blade.php
@@ -1,106 +1,26 @@
-<?php
-namespace App\Http\Controllers;
-use App\Answer;
-use App\Question;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\Request;
-class AnswerController extends Controller
-{
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create($question)
-    {
-        $answer = new Answer;
-        $edit = FALSE;
-        return view('answerForm', ['answer' => $answer,'edit' => $edit, 'question' =>$question  ]);
-    }
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request, $question)
-    {
-        $input = $request->validate([
-            'body' => 'required|min:5',
-        ], [
-            'body.required' => 'Body is required',
-            'body.min' => 'Body must be at least 5 characters',
-        ]);
-        $input = request()->all();
-        $question = Question::find($question);
-        $Answer = new Answer($input);
-        $Answer->user()->associate(Auth::user());
-        $Answer->question()->associate($question);
-        $Answer->save();
-        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
-    }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($question,  $answer)
-    {
-        $answer = Answer::find($answer);
-        return view('answer')->with(['answer' => $answer, 'question' => $question]);
-    }
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($question,  $answer)
-    {
-        $answer = Answer::find($answer);
-        $edit = TRUE;
-        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question'=>$question ]);
-    }
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $question, $answer)
-    {
-        $input = $request->validate([
-            'body' => 'required|min:5',
-        ], [
-            'body.required' => 'Body is required',
-            'body.min' => 'Body must be at least 5 characters',
-        ]);
-        $answer = Answer::find($answer);
-        $answer->body = $request->body;
-        $answer->save();
-        return redirect()->route('answers.show',['question_id' => $question, 'answer_id' => $answer])->with('message', 'Updated');
-    }
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($question, $answer)
-    {
-        $answer = Answer::find($answer);
-        $answer->delete();
-        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Deleted');
-    }
-}
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Answer</div>
+                    <div class="card-body">
+                        {{$answer->body}}
+                    </div>
+                    <div class="card-footer">
+                        {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
+                        </button>
+                        {!! Form::close() !!}
+                        <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
+                            Edit Answer
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/answerform.blade.php
+++ resources/views/answerform.blade.php
@@ -5,24 +5,21 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Create Answer</div>
+                    <div class="card-header">Answer</div>
                     <div class="card-body">
-                        @if($edit === FALSE)
-                            {!! Form::model($answer, ['route' => ['answers.store', $question], 'method' => 'post']) !!}
-
-                        @else()
-                            {!! Form::model($answer, ['route' => ['answers.update', $question, $answer], 'method' => 'patch']) !!}
-                        @endif
-                        <div class="form-group">
-                            {!! Form::label('body', 'Body') !!}
-                            {!! Form::text('body', $answer->body, ['class' => 'form-control','required' => 'required']) !!}
-                        </div>
-                        <button class="btn btn-success float-right" value="submit" type="submit" id="submit">Save
+                        {{$answer->body}}
+                    </div>
+                    <div class="card-footer">
+                        {{ Form::open(['method'  => 'DELETE', 'route' => ['answers.destroy', $question, $answer->id]])}}
+                        <button class="btn btn-danger float-right mr-2" value="submit" type="submit" id="submit">Delete
                         </button>
                         {!! Form::close() !!}
+                        <a class="btn btn-primary float-right" href="{{ route('answers.edit',['question_id'=> $question, 'answer_id'=> $answer->id, ])}}">
+                            Edit Answer
+                        </a>
                     </div>
-
                 </div>
+
             </div>
         </div>
     </div>
